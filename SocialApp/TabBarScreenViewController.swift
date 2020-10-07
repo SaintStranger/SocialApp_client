8af6c44c736e7b0ceccfac6feae5cf2b23fba860
Antony Chechevichkin <Anton-bopoh@yandex.ru>
2020-10-07 14:34:36 +0300

    Домашняя работа

---
+++ SocialApp/TabBarScreenViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarScreenViewController.swift
+//  SocialApp
+//
+//  Created by Антон Чечевичкин on 07.10.2020.
+//
+
+import UIKit
+
+class TabBarScreenViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
